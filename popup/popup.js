73f54cade6497f125d73dca61cffba7ee257e260
Its-A-Mia <miapurdun@gmail.com>
2023-03-26 22:50:20 -0500

    added more of the general shape to the extension popup

---
+++ popup/popup.js
@@ -0,0 +1,15 @@
+// background scripts //
+// check cache
+// if no cache, then fetch
+// call fetch and recache at interval
+// column-item constructor function
+// filter functionality:
+// settings functionality: light/dark mode, show/hide certain info like betting odds or stats
+// column functionality: style a card as 'primary'(lock as focused card), pin match card
+
+//general technique for snagging info, then maybe doing something with it
+// fetch("https://example.com/api/blah")
+//     .then(response => response.json())
+//     .then(data => {
+
+//     })
